1f94e2c5214483c903c5c127fc77b2ecf5424c72
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-04-17 18:42:14 +0200

    t

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,11 +6,11 @@ import { Filter } from "./Filter/filter";
 import { fetchContacts } from "redux/operations";
 import { useDispatch, useSelector } from "react-redux";
 import { selectContacts, selectIsLoading, selectError } from "redux/selectors";
-
 import { Home } from '../pages/Home';
 import { Login } from '../pages/Login';
 import { Register } from '../pages/Register';
 import { ContactsPage } from '../pages/ContactsPage';
+import { Header } from "./Header/header";
 
 export function App() {
   const dispatch = useDispatch();
@@ -24,17 +24,14 @@ export function App() {
 
   return (
     <div>
-      <div>
-      {/* <AppBar /> */}
-
       <Routes>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={<Header/>} />
+        <Route exact index component={Home} />
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
         <Route path="/contacts" component={ContactsPage} />
-        </Routes>
-        </div>
-
+      </Routes>
+        
       <h2>Phonebook</h2>
       <ContactForm />
       <Filter />
--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
+
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
