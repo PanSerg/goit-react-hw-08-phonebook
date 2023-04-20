815c96cc65f8eaf1a24aeb28726e1c83c9f00202
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-04-20 21:48:25 +0200

    update

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -19,7 +19,7 @@ const authSlice = createSlice({
     },
     [logIn.fulfilled](state, action) {
       state.user = action.payload.user;
-      state.token = action.payload.user;
+      state.token = action.payload.token;
       state.isLoggedIn = true;
       },
     [logOut.fulfilled](state) {
